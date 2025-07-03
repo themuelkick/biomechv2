bd2b8c5716a2f29040d60190172de89bf2e92327
themuelkick <mmueller4@rogers.com>
2025-07-03 18:30:15 -0400

    Add files via upload
    adding supabase logic. before adding code to upload session so the files go to supabase

--- app.py
+++ app.py
@@ -1,10 +1,74 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-import os
 from datetime import datetime
 import re
 import plotly.graph_objects as go
+from dotenv import load_dotenv
+from supabase import create_client, Client
+import os
+
+# authentication
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)# log in page end
+
+def sign_up(email, password):
+    try:
+        user = supabase.auth.sign_up({"email": email, "password": password})
+        return user
+    except Exception as e:
+        st.error(f"Login Failed: {e}")
+
+def sign_in(email, password):
+    try:
+        user = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        return user
+    except Exception as e:
+        st.error(f"Registration Failed: {e}")
+
+def sign_out():
+    try:
+        supabase.auth.sign_out()
+        st.session_state.user_email = None
+        st.rerun()
+    except Exception as e:
+        st.error(f"Logout failed: {e}")
+
+def main_app(user_email):
+    st.title("Welcome Page")
+    st.success(f"Welcome, {user_email}!")
+    if st.button("Logout"):
+        sign_out()
+
+def auth_screen():
+    st.title("Streamlit Auth App")
+    option = st.selectbox(" Choose an Action:", ["Login", "Sign Up"])
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+
+    if option == "Sign Up" and st.button("Register"):
+        user = sign_up(email, password)
+        if user and user.user:
+            st.success("Registration successful. Please log in.")
+
+    if option == "Login" and st.button("Login"):
+        user = sign_in(email, password)
+        if user and user.user:
+            st.session_state.user_email = user.user.email
+            st.success(f"Welcome back, {email}")
+            st.rerun()
+
+if "user_email" not in st.session_state:
+    st.session_state.user_email = None
+
+if st.session_state.user_email:
+    main_app(st.session_state.user_email)
+else:
+    auth_screen()
+# end of authentication
 
 VIDEO_DIR = "videos"
 os.makedirs(VIDEO_DIR, exist_ok=True)
